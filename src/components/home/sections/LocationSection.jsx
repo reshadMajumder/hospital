6359781edaa0771166ce80ec7ca3bb027ebb496e
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-12-25 01:27:17 +0600

    map added

--- src/components/home/sections/LocationSection.jsx
+++ src/components/home/sections/LocationSection.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import GoogleMapComponent from '../../maps/GoogleMapComponent';
 import axios from 'axios';
 import API_URL from '../../../data/ApiData';
 
@@ -54,7 +53,16 @@ function LocationSection() {
           </Col>
           <Col lg={6} className="d-flex">
             <div className="map-container w-100 rounded overflow-hidden shadow" style={{ height: "300px" }}>
-              <GoogleMapComponent />
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d227.14782093158726!2d91.41192960623512!3d24.3685982015751!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37514529fb0ed5a7%3A0xca8eb75a9cbfa8c7!2z4Kaq4Kaq4KeB4Kay4Ka-4KawIOCmnOCnh-CmqOCmvuCmsOCnh-CmsiDgprngpr7gprjgpqrgpr7gpqTgpr7gprI!5e0!3m2!1sen!2sbd!4v1735068235644!5m2!1sen!2sbd"
+                
+                width="100%"
+                height="100%"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
             </div>
           </Col>
         </Row>
