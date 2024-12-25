1b913c39906f7266a525b0d70fd57b87bf52664c
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-12-25 12:15:32 +0600

    url update

--- src/data/ApiData.js
+++ src/data/ApiData.js
@@ -1,13 +1,13 @@
 // for production
 
-// const API_URL = 'https://exoverse.pythonanywhere.com';   
+const API_URL = 'https://exoverse.pythonanywhere.com';   
 
 
 
 
 // for development
 
-const API_URL = 'http://127.0.0.1:8000';   
+// const API_URL = 'http://127.0.0.1:8000';   
 
 
 
